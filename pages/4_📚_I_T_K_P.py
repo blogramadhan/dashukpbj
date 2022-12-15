1653b162ad0c7ecf0aa208472d3207de37dfee1d
Kurnia Ramadhan <kramadhan@gmail.com>
2022-12-16 06:53:59 +0700

    update itkp

--- pages/4_📚_I_T_K_P.py
+++ pages/4_📚_I_T_K_P.py
@@ -65,11 +65,51 @@ tab1, tab2 = st.tabs(["ITKP UKPBJ", "ITKP PD"])
 # Tab ITKP UKPBJ
 with tab1:
 
+    ## Dataset ITKP UKPBJ
+    DatasetSIRUPDP = f"data/ITKP/{kodeFolder}/sirupdp{str(tahun)}.parquet"
+    DatasetSIRUPSW = f"data/ITKP/{kodeFolder}/sirupsw{str(tahun)}.parquet"
+    DatasetSIRUPDSARSAP = f"data/ITKP/{kodeFolder}/sirupdsa_rsap{str(tahun)}.parquet"
+    DatasetTENDERDTS = f"data/ITKP/{kodeFolder}/dtender_dts{str(tahun)}.parquet"
+    DatasetTENDERDTKS = f"data/ITKP/{kodeFolder}/dtender_dtks{str(tahun)}.parquet"
+    DatasetNTENDERDNTS = f"data/ITKP/{kodeFolder}/dntender_dnts{str(tahun)}.parquet"
+    DatasetKATALOG = f""
+
+    ### Data RUP paket penyedia
+
+    ### Data RUP paket swakelola
+
+    ### Data struktur anggaran RUP
+    df_rsap = pd.read_parquet(DatasetSIRUPDSARSAP)
+
+    ### Data Tender
+
+    ### Data Non Tender
+
+    ### Data Katalog
+
     ## Mulai tampilkan data ITKP UKPBJ
     st.subheader(f"DATA ITKP TAHUN {tahun} - {pilih}")
 
+    ## Tampilan pemanfaatan SIRUP
+    st.markdown(f"## **PEMANFAATAN SIRUP**")
+    ### RUP struktur anggaran
+    st.markdown(f"### Struktur Anggaran")
+    belanja_pengadaan = df_rsap['belanja_pengadaan'].sum()
+    belanja_pengadaan_print = format_currency(belanja_pengadaan, 'Rp. ', locale='id_ID')
+    belanja_operasional = df_rsap['belanja_operasi'].sum()
+    belanja_operasional_print = format_currency(belanja_operasional, 'Rp. ', locale='id_ID')
+    belanja_modal = df_rsap['belanja_modal'].sum()
+    belanja_modal_print = format_currency(belanja_modal, 'Rp. ', locale='id_ID')
+
+    sa1, sa2, sa3 = st.columns(3)
+    sa1.metric("Belanja Pengadaan", belanja_pengadaan_print)
+    sa2.metric("Belanja Operasional", belanja_operasional_print)
+    sa3.metric("Belanja Modal", belanja_modal_print)
+
 # Tab ITKP PD
 with tab2:
+
+    ## Dataset ITKP PD
 
     ## Mulai tampilkan data ITKP Perangkat Daerah
     st.subheader(f"DATA ITKP TAHUN {tahun} - PERANGKAT DAERAH")
