0f02ca5baf3800997c3209194f55cdd9b15a4db5
Kurnia Ramadhan <kramadhan@gmail.com>
2022-12-16 21:29:53 +0700

    update data

--- pages/2_📚_S_I_R_U_P.py
+++ pages/2_📚_S_I_R_U_P.py
@@ -65,11 +65,6 @@ con = duckdb.connect()
 DatasetSIRUPDP = f"data/ITKP/{kodeFolder}/sirupdp{str(tahun)}.parquet"
 DatasetSIRUPSW = f"data/ITKP/{kodeFolder}/sirupdsw{str(tahun)}.parquet"
 DatasetSIRUPDSARSAP = f"data/ITKP/{kodeFolder}/sirupdsa_rsap{str(tahun)}.parquet"
-#DatasetTENDERDTS = f"data/ITKP/{kodeFolder}/dtender_dts{str(tahun)}.parquet"
-#DatasetTENDERDTKS = f"data/ITKP/{kodeFolder}/dtender_dtks{str(tahun)}.parquet"
-#DatasetNTENDERDNTS = f"data/ITKP/{kodeFolder}/dntender_dnts{str(tahun)}.parquet"
-#DatasetKATALOG = f"data/ePurchasing/{kodeFolder}/trxkatalog{str(tahun)}.parquet"
-#DatasetDARING = f"data/ePurchasing/{kodeFolder}/daring{str(tahun)}.parquet"
 
 ### Query Data RUP paket penyedia
 df_pp = pd.read_parquet(DatasetSIRUPDP)
