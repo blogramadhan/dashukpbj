6704f64f7f58271ae8cca39ac12ddf2f5e90aa29
Kurnia Ramadhan <kramadhan@gmail.com>
2022-12-18 06:39:27 +0700

    update data

--- pages/2_📚_S_I_R_U_P.py
+++ pages/2_📚_S_I_R_U_P.py
@@ -1,3 +1,24 @@
+#####################################################################################
+# Source code: Dashboard analisa data Pengadaan Barang dan Jasa di Kalimantan Barat #
+#-----------------------------------------------------------------------------------#
+# Dashboard ini dibuat oleh:                                                        #
+# Nama          : Kurnia Ramadhan, ST.,M.Eng                                        #
+# Jabatan       : Sub Koordinator Pengelolaan Informasi LPSE                        #
+# Instansi      : Biro Pengadaan Barang dan Jasa Setda Prov. Kalbar                 #
+# Email         : kramadhan@gmail.com                                               #
+# URL Web       : https://github.com/blogramadhan                                   #
+#-----------------------------------------------------------------------------------#
+# Hak cipta milik Allah SWT, source code ini silahkan dicopy, di download atau      #
+# di distribusikan ke siapa saja untuk bahan belajar, atau untuk dikembangkan lagi  #
+# lebih lanjut, btw tidak untuk dijual ya.                                          #
+#                                                                                   #
+# Jika teman-teman mengembangkan lebih lanjut source code ini, agar berkenan untuk  #
+# men-share code yang teman-teman kembangkan lebih lanjut sebagai bahan belajar     #
+# untuk kita semua.                                                                 #
+#-----------------------------------------------------------------------------------#
+# @ Pontianak, 2022                                                                 #
+#####################################################################################
+
 import duckdb
 import streamlit as st
 import pandas as pd
