97626a6aebfe1535dc6bfb7dcff32b67ae9dc07b
Kurnia Ramadhan <kramadhan@gmail.com>
2022-12-18 19:46:52 +0700

    update data

--- pages/2_📚_S_I_R_U_P.py
+++ pages/2_📚_S_I_R_U_P.py
@@ -207,7 +207,7 @@ with tab1:
     with mpn2:
         st.markdown(":")
     with mpn3:
-        figmpn = px.pie(df_mp_nilai, values='NILAI_PENGADAAN', names='METODE_PENGADAAN', title='Grafik Metode Pengadaan - Nilai Paket', hole=.3, width=100%, height=100%)
+        figmpn = px.pie(df_mp_nilai, values='NILAI_PENGADAAN', names='METODE_PENGADAAN', title='Grafik Metode Pengadaan - Nilai Paket', hole=.3, width=800, height=800)
         st.plotly_chart(figmpn, theme='streamlit', use_container_width=True)
 
 with tab2:
