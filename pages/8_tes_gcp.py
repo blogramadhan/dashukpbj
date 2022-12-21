c5d4ffb3be57ceef24f5fbff4a6d74328b797b35
Kurnia Ramadhan <kramadhan@gmail.com>
2022-12-21 18:50:42 +0700

    gcp update

---
+++ pages/8_tes_gcp.py
@@ -0,0 +1,29 @@
+# streamlit_app.py
+
+import streamlit as st
+from google.oauth2 import service_account
+from google.cloud import storage
+
+# Create API client.
+credentials = service_account.Credentials.from_service_account_info(
+    st.secrets["gcp_service_account"]
+)
+client = storage.Client(credentials=credentials)
+
+# Retrieve file contents.
+# Uses st.experimental_memo to only rerun when the query changes or after 10 min.
+@st.experimental_memo(ttl=600)
+def read_file(bucket_name, file_path):
+    bucket = client.bucket(bucket_name)
+    content = bucket.blob(file_path).download_as_string().decode("utf-8")
+    return content
+
+bucket_name = "streamlit-bucket"
+file_path = "myfile.csv"
+
+content = read_file(bucket_name, file_path)
+
+# Print results.
+for line in content.strip().split("\n"):
+    name, pet = line.split(",")
+    st.write(f"{name} has a :{pet}:")
