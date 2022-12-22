c79c18a1cc375a37a1083c9933bca98b62a91e41
Kurnia Ramadhan <kramadhan@gmail.com>
2022-12-22 19:51:35 +0700

    update data

--- pages/7_📚_Kontak_Saya.py
+++ pages/7_📚_Kontak_Saya.py
@@ -62,19 +62,19 @@ def read_file(bucket_name, file_path):
 bucket_name = "ular_kadut"
 
 con = duckdb.connect()
-#DatasetSIRUPDP_Path = "sirupdp2023.parquet"
-#DatasetSIRUPDP = read_file(bucket_name, DatasetSIRUPDP_Path)
-#df_pp_umumkan = con.execute(
-#    f"SELECT * FROM '{DatasetSIRUPDP}' WHERE statusumumkan = 'Terumumkan'"
-#).df()
-#df_mp_hitung = con.execute(
-#    "SELECT metodepengadaan AS METODE_PENGADAAN, COUNT(metodepengadaan) AS JUMLAH_PAKET FROM df_pp_umumkan WHERE metodepengadaan IS NOT NULL GROUP BY metodepengadaan;"
-#).df()
+DatasetSIRUPDP_Path = "sirupdp2023.parquet"
+DatasetSIRUPDP = read_file(bucket_name, DatasetSIRUPDP_Path)
+df_pp_umumkan = con.execute(
+    f"SELECT * FROM '{DatasetSIRUPDP}' WHERE statusumumkan = 'Terumumkan'"
+).df()
+df_mp_hitung = con.execute(
+    "SELECT metodepengadaan AS METODE_PENGADAAN, COUNT(metodepengadaan) AS JUMLAH_PAKET FROM df_pp_umumkan WHERE metodepengadaan IS NOT NULL GROUP BY metodepengadaan;"
+).df()
 
 #################
 file_path = "myfile.csv"
 content = read_file(bucket_name, file_path)
-content_print = con.execute(f"SELECT * FROM {content}").df()
+#content_print = con.execute(f"SELECT * FROM {content}").df()
 #################
 
 st.markdown("## Tes Google Cloud Storage")
