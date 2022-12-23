fb17b3fed178fbf170ba1e33c8ffb6770c70dbb5
Kurnia Ramadhan <kramadhan@gmail.com>
2022-12-23 20:01:39 +0700

    update data

--- 1_🤓_Halaman_Awal.py
+++ 1_🤓_Halaman_Awal.py
@@ -43,13 +43,27 @@ st.markdown(hide_st_style, unsafe_allow_html=True)
 st.title("Dashboard Pengadaan Barang dan Jasa")
 
 st.markdown("""
-Dashboard ini dibuat sebagai alat bantu untuk mempermudah para pelaku pengadaan di seluruh wilayah Provinsi Kalimantan Barat. Data yang disajikan, antara lain:
+# Dashboard Pengadaan Barang dan Jasa
 
-* Rencana PBJ
+*Dashboard* ini dibuat sebagai alat bantu untuk mempermudah para pelaku pengadaan di seluruh wilayah Provinsi Kalimantan Barat. Data yang disajikan, antara lain:
+
+* **Rencana PBJ**
+  * *RUP Daerah*
+  * *Struktur Anggaran*
+  * *RUP Perangkat Daerah*
+  * *RUP Paket Penyedia*
+  * *RUP Paket Swakelola*
 * Realisasi PBJ
 * Indeks Tata Kelola PBJ
 
-Sumber data dari Dashboard ini berasal dari **API JSON** yang ditarik harian dari [ISB LKPP](https://lkpp.go.id).
+*Made with love* dengan menggunakan bahasa programming [Python](https://www.python.org/) dengan beberapa *library* utama seperti:
+* [Pandas](https://pandas.pydata.org/)
+* [Streamlit](https://streamlit.io)
+* [DuckDB](https://duckdb.org)
+
+Sumber data dari *Dashboard* ini berasal dari **API JSON** yang ditarik harian dari [ISB LKPP](https://lkpp.go.id). Data tersebut kemudian disimpan di [Google Cloud Storage](https://google.com) untuk kemudian diolah lebih lanjut dengan [Python](https://python.org).
+
+@2022 - **UlarKadut** 
 """)
 
 # Buat session pilihan UKPBJ
