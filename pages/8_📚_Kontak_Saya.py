6ccbe06a45227223d406635ba267efc695f7fb22
Kurnia Ramadhan <kramadhan@gmail.com>
2022-12-23 21:32:16 +0700

    update data

--- pages/8_📚_Kontak_Saya.py
+++ pages/8_📚_Kontak_Saya.py
@@ -22,6 +22,10 @@
 # Import library
 import streamlit as st
 
+from google.oauth2 import service_account
+from google.cloud import storage
+
+
 # Setting CSS
 with open('style.css') as f:
     st.markdown(f"<style>{f.read()}</style>", unsafe_allow_html=True)
@@ -36,3 +40,28 @@ hide_st_style = """
 st.markdown(hide_st_style, unsafe_allow_html=True)
 
 st.title("Kontak Saya")
+
+#### Tes Google Cloud Storage
+
+# Create API client.
+credentials = service_account.Credentials.from_service_account_info(
+    st.secrets["gcp_service_account"]
+)
+client = storage.Client(credentials=credentials)
+
+# Retrieve file contents.
+# Uses st.experimental_memo to only rerun when the query changes or after 10 min.
+@st.experimental_memo(ttl=600)
+def read_file(bucket_name, file_path):
+    bucket = client.bucket(bucket_name)
+    return bucket.blob(file_path).download_as_string().decode("utf-8")
+
+bucket_name = "dashukpbj"
+file_path = "myfile.csv"
+
+content = read_file(bucket_name, file_path)
+
+# Print results.
+for line in content.strip().split("\n"):
+    name, pet = line.split(",")
+    st.write(f"{name} has a :{pet}:")
