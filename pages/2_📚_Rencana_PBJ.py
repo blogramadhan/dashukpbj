4c89e55488e7336cab6a6a09273e79f26ab1ddc3
Kurnia Ramadhan <kramadhan@gmail.com>
2022-12-23 08:34:52 +0700

    update data

--- pages/2_📚_Rencana_PBJ.py
+++ pages/2_📚_Rencana_PBJ.py
@@ -218,10 +218,10 @@ with tab1:
     mph1, mph2 = st.columns((5,5))
     with mph1:
         st.markdown("#### Berdasarkan Jumlah Paket")
-        st.table(df_mp_hitung)
+        AgGrid(df_mp_hitung)
     with mph2:
         st.markdown("#### Berdasarkan Nilai Paket")
-        st.table(df_mp_nilai)
+        AgGrid(df_mp_nilai)
  
     mpn1, mpn2 = st.columns((5,5))
     with mpn1:
@@ -237,10 +237,10 @@ with tab1:
     jph1, jph2 = st.columns((5,5))
     with jph1:
         st.markdown("#### Berdasarkan Jumlah Paket")
-        st.table(df_jp_hitung)
+        AgGrid(df_jp_hitung)
     with jph2:
         st.markdown("#### Berdasarkan Nilai Paket")
-        st.table(df_jp_nilai)
+        AgGrid(df_jp_nilai)
 
     jpn1, jpn2 = st.columns((5,5))
     with jpn1:
