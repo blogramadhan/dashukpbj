082748ff0967955af141de2896a6603b13613b55
Kurnia Ramadhan <kramadhan@gmail.com>
2023-01-26 09:09:47 +0700

    update rencana pbj

--- pages/2_📚_Rencana_PBJ.py
+++ pages/2_📚_Rencana_PBJ.py
@@ -158,7 +158,7 @@ try:
     namaopd = df_SIRUPDP['namasatker'].unique()
 
 except Exception:
-    st.error(f"Data SIRUP belum ada atau gagal download ... {DatasetSIRUPDSW_URL} ")
+    st.error(f"Data SIRUP belum ada atau gagal download ... {df_SIRUPDSW.head(1)} ")
 
 #########
 
