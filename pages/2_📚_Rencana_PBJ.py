501e9e3f65fc9b6d372b97354ba15a75eda74995
Kurnia Ramadhan <kramadhan@gmail.com>
2023-03-16 12:28:20 +0700

    update data

--- pages/2_📚_Rencana_PBJ.py
+++ pages/2_📚_Rencana_PBJ.py
@@ -568,14 +568,14 @@ with tab6:
     #tb_persenrup = con.execute("SELECT tb_datapenyedia.nama_satker AS nama_satker, tb_datapenyedia.jumlah_pagu_penyedia AS penyedia, tb_dataswakelola.jumlah_pagu_swakelola AS swakelola FROM tb_datapenyedia FULL OUTER JOIN JOIN tb_dataswakelola ON tb_datapenyedia.nama_satker = tb_dataswakelola.nama_satker").df()
 
     ### Tabulasi data dan pagination AgGrid
-    gd = GridOptionsBuilder.from_dataframe(tb_gabung)
+    gd = GridOptionsBuilder.from_dataframe(tb_gabung_hitung)
     gd.configure_pagination()
     gd.configure_side_bar()
     gd.configure_default_column(groupable=True, value=True, enableRowGroup=True, aggFunc="sum", editable=True)
-    gd.configure_column("Struktur Anggaran", type=["numericColumn", "numberColumnFilter", "customNumericFormat"], valueGetter = "data.belanja_pengadaan.toLocaleString('id-ID', {style: 'currency', currency: 'IDR', maximumFractionDigits:2})")
-    gd.configure_column("RUP Penyedia", type=["numericColumn", "numberColumnFilter", "customNumericFormat"], valueGetter = "data.jumlah_pagu_penyedia.toLocaleString('id-ID', {style: 'currency', currency: 'IDR', maximumFractionDigits:2})")
-    gd.configure_column("RUP Swakelola", type=["numericColumn", "numberColumnFilter", "customNumericFormat"], valueGetter = "data.jumlah_pagu_swakelola.toLocaleString('id-ID', {style: 'currency', currency: 'IDR', maximumFractionDigits:2})")
+    #gd.configure_column("Struktur Anggaran", type=["numericColumn", "numberColumnFilter", "customNumericFormat"], valueGetter = "data.belanja_pengadaan.toLocaleString('id-ID', {style: 'currency', currency: 'IDR', maximumFractionDigits:2})")
+    #gd.configure_column("RUP Penyedia", type=["numericColumn", "numberColumnFilter", "customNumericFormat"], valueGetter = "data.jumlah_pagu_penyedia.toLocaleString('id-ID', {style: 'currency', currency: 'IDR', maximumFractionDigits:2})")
+    #gd.configure_column("RUP Swakelola", type=["numericColumn", "numberColumnFilter", "customNumericFormat"], valueGetter = "data.jumlah_pagu_swakelola.toLocaleString('id-ID', {style: 'currency', currency: 'IDR', maximumFractionDigits:2})")
 
     gridOptions = gd.build()
 
-    AgGrid(tb_gabung, gridOptions=gridOptions, enable_enterprise_modules=True)
+    AgGrid(tb_gabung_hitung, gridOptions=gridOptions, enable_enterprise_modules=True)
